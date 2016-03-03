4c1dc24f9422aaa334daf1caeb1344955f66b61b
Pat Yolian <pyolian@gmail.com>
2016-03-02 18:48:24 -0800

    Add body text and comments

--- class-01-jquery-and-the-dom/taylor-pat/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/taylor-pat/starter-code/scripts/article.js
@@ -13,14 +13,12 @@ Article.prototype.toHtml = function () {
   var $newArticle = $('article.template').clone();
 
   $newArticle.attr('data-category', this.category);
-
-  // TODO: Use jQuery to fill in the template with properties
-  // from this particular Article instance. We need to fill in:
-  // the author name and url, the article title and body, and the
-  // publication date.
-
+  //Include title in h1 tag
   $newArticle.find('h1').text(this.title);
+  //Include author name in link tag and set href attribute to authorURL
   $newArticle.find('.byline a').text(this.author).attr('href', this.authorUrl);
+  //Include text into .article-body class
+  $newArticle.find('section.article-body').html(this.body);
 
   // Include the publication date as a 'title' attribute to show on hover:
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
@@ -30,7 +28,6 @@ Article.prototype.toHtml = function () {
 
   $newArticle.append('<hr>');
 
-  // TODO: This cloned article is no longer a template, so we should remove that class...
   $newArticle.removeClass('template');
   return $newArticle;
 };
