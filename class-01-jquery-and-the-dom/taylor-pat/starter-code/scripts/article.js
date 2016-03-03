5e8199b6a675624b8eaad1e0d575f62f5d5f86ef
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-03-02 19:12:43 -0800

    add eslint file and make changes

--- class-01-jquery-and-the-dom/taylor-pat/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/taylor-pat/starter-code/scripts/article.js
@@ -13,10 +13,13 @@ Article.prototype.toHtml = function () {
   var $newArticle = $('article.template').clone();
 
   $newArticle.attr('data-category', this.category);
+
   //Include title in h1 tag
   $newArticle.find('h1').text(this.title);
+
   //Include author name in link tag and set href attribute to authorURL
   $newArticle.find('.byline a').text(this.author).attr('href', this.authorUrl);
+
   //Include text into .article-body class
   $newArticle.find('section.article-body').html(this.body);
 
