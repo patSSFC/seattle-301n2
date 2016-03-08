83deec16840511a40682f75028ac0cddc3b3bd6f
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-03-07 19:09:16 -0800

    linting and touchups

--- class-03-responsive-mobile-first/pair-assignment/pat-taylor/starter-code/scripts/article.js
+++ class-03-responsive-mobile-first/pair-assignment/pat-taylor/starter-code/scripts/article.js
@@ -1,6 +1,6 @@
 var articles = [];
 
-function Article (opts) {
+function Article(opts) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
   this.title = opts.title;
@@ -9,34 +9,35 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function () {
   var $newArticle = $('article.template').clone();
   $newArticle.removeClass('template');
   if (!this.publishedOn) {
     $newArticle.addClass('draft');
   }
+
   $newArticle.attr('data-category', this.category);
-  $newArticle.attr('data-author', this.author)
+  $newArticle.attr('data-author', this.author);
 
   $newArticle.find('.byline a').html(this.author);
   $newArticle.find('.byline a').attr('href', this.authorUrl);
   $newArticle.find('h1:first').html(this.title);
   $newArticle.find('.article-body').html(this.body);
-  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn)
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
-}
+};
 
-rawData.sort(function(a,b) {
+rawData.sort(function (a, b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-rawData.forEach(function(ele) {
+rawData.forEach(function (ele) {
   articles.push(new Article(ele));
-})
+});
 
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+articles.forEach(function (a) {
+  $('#articles').append(a.toHtml());
 });
--- class-03-responsive-mobile-first/pair-assignment/pat-taylor/starter-code/scripts/articleView.js
+++ class-03-responsive-mobile-first/pair-assignment/pat-taylor/starter-code/scripts/articleView.js
@@ -1,8 +1,8 @@
 // Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
 var articleView = {};
 
-articleView.populateFilters = function() {
-  $('article').each(function() {
+articleView.populateFilters = function () {
+  $('article').each(function () {
     if (!$(this).hasClass('template')) {
       var val = $(this).find('address a').text();
       var optionTag = '<option value="' + val + '">' + val + '</option>';
@@ -17,8 +17,8 @@ articleView.populateFilters = function() {
   });
 };
 
-articleView.handleAuthorFilter = function() {
-  $('#author-filter').on('change', function() {
+articleView.handleAuthorFilter = function () {
+  $('#author-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $('article[data-author="' + $(this).val() + '"]').fadeIn();
@@ -26,12 +26,13 @@ articleView.handleAuthorFilter = function() {
       $('article').fadeIn();
       $('article.template').hide();
     }
+
     $('#category-filter').val('');
   });
 };
 
-articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
+articleView.handleCategoryFilter = function () {
+  $('#category-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $('article[data-category="' + $(this).val() + '"]').fadeIn();
@@ -39,12 +40,13 @@ articleView.handleCategoryFilter = function() {
       $('article').fadeIn();
       $('article.template').hide();
     }
+
     $('#author-filter').val('');
   });
 };
 
-articleView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function(e) {
+articleView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function (e) {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
   });
@@ -52,20 +54,20 @@ articleView.handleMainNav = function() {
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
-articleView.setTeasers = function() {
+articleView.setTeasers = function () {
   $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
 
-  $('#articles').on('click', 'a.read-on', function(e) {
+  $('#articles').on('click', 'a.read-on', function (e) {
     e.preventDefault();
     $(this).parent().find('*').fadeIn();
     $(this).hide();
   });
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
   articleView.handleMainNav();
   articleView.setTeasers();
-})
+});
