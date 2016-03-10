f633dd4ca79c12152468693520feacac63a28cbd
Pat Yolian <pyolian@gmail.com>
2016-03-09 19:16:44 -0800

    Fix missing author name and published date. Add template for fitlers

--- class-04-templates/pair-assignment/pat-taylor/scripts/article.js
+++ class-04-templates/pair-assignment/pat-taylor/scripts/article.js
@@ -10,9 +10,7 @@ function Article(opts) {
 }
 
 Article.prototype.toHtml = function () {
-  // TODO: Use handlebars to render your articles.
-  //       - Get your template from the DOM.
-  //       - Now "compile" your template with Handlebars.
+  
 
   $source = $('#article-template').html();
   $template = Handlebars.compile($source);
@@ -24,7 +22,7 @@ Article.prototype.toHtml = function () {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+
   var html = $template(this);
   return html;
 };
