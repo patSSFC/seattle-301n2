7f274c5a19cfdf6610d88e42983a413f4ba4d38a
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-03-09 18:57:40 -0800

    pass each article into handlebars to render

--- class-04-templates/pair-assignment/pat-taylor/scripts/article.js
+++ class-04-templates/pair-assignment/pat-taylor/scripts/article.js
@@ -1,6 +1,6 @@
 var articles = [];
 
-function Article (opts) {
+function Article(opts) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
   this.title = opts.title;
@@ -9,29 +9,34 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function () {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
+
+  $source = $('#article-template').html();
+  $template = Handlebars.compile($source);
 
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  var html = $template(this);
+  return html;
 };
 
-rawData.sort(function(a,b) {
+rawData.sort(function (a, b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-rawData.forEach(function(ele) {
+rawData.forEach(function (ele) {
   articles.push(new Article(ele));
-})
+});
 
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+articles.forEach(function (a) {
+  $('#articles').append(a.toHtml());
 });
