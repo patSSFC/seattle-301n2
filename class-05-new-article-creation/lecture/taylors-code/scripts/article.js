792b565e91dbc717455509a51e846b857a822113
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-03-13 09:54:02 -0700

    in class workshop

--- class-05-new-article-creation/lecture/taylors-code/scripts/article.js
+++ class-05-new-article-creation/lecture/taylors-code/scripts/article.js
@@ -1,6 +1,6 @@
 var articles = [];
 
-function Article (opts) {
+function Article(opts) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
   this.title = opts.title;
@@ -9,26 +9,26 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function () {
   var template = Handlebars.compile($('#article-template').text());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   this.body = marked(this.body);
-  
+
   return template(this);
 };
 
 if (typeof rawData !== 'undefined') {
-  rawData.sort(function(a,b) {
+  rawData.sort(function (a, b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
   });
 
-  rawData.forEach(function(ele) {
+  rawData.forEach(function (ele) {
     articles.push(new Article(ele));
-  })
+  });
 }
 
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+articles.forEach(function (a) {
+  $('#articles').append(a.toHtml());
 });
--- class-05-new-article-creation/lecture/taylors-code/scripts/articleView.js
+++ class-05-new-article-creation/lecture/taylors-code/scripts/articleView.js
@@ -1,8 +1,8 @@
 // Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
 var articleView = {};
 
-articleView.populateFilters = function() {
-  $('article').each(function() {
+articleView.populateFilters = function () {
+  $('article').each(function () {
     if (!$(this).hasClass('template')) {
       var val = $(this).find('address a').text();
       var optionTag = '<option value="' + val + '">' + val + '</option>';
@@ -17,8 +17,8 @@ articleView.populateFilters = function() {
   });
 };
 
-articleView.handleAuthorFilter = function() {
-  $('#author-filter').on('change', function() {
+articleView.handleAuthorFilter = function () {
+  $('#author-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $('article[data-author="' + $(this).val() + '"]').fadeIn();
@@ -26,12 +26,13 @@ articleView.handleAuthorFilter = function() {
       $('article').fadeIn();
       $('article.template').hide();
     }
+
     $('#category-filter').val('');
   });
 };
 
-articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
+articleView.handleCategoryFilter = function () {
+  $('#category-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $('article[data-category="' + $(this).val() + '"]').fadeIn();
@@ -39,12 +40,13 @@ articleView.handleCategoryFilter = function() {
       $('article').fadeIn();
       $('article.template').hide();
     }
+
     $('#author-filter').val('');
   });
 };
 
-articleView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function(e) {
+articleView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function (e) {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
   });
@@ -52,40 +54,62 @@ articleView.handleMainNav = function() {
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
-articleView.setTeasers = function() {
+articleView.setTeasers = function () {
   $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
 
-  $('#articles').on('click', 'a.read-on', function(e) {
+  $('#articles').on('click', 'a.read-on', function (e) {
     e.preventDefault();
     $(this).parent().find('*').fadeIn();
     $(this).hide();
   });
 };
 
-articleView.initNewArticlePage = function() {
+articleView.initNewArticlePage = function () {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later.
+  $('.tab-content').show();
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
+  $('#export-field').hide();
+  $('#article-json').on('focus', function () {
+    //this selectes the actual text on the page to make it easier to cpoy / paste
+    this.select();
+  });
 
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  $('#new-form').on('change', 'input, textarea', articleView.create);
 };
 
-articleView.create = function() {
+articleView.create = function () {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
+  var articleData;
+  $('#articles').empty();
 
   // TODO: Instantiate an article based on what's in the form fields:
+  articleData = new Article({
+    title: $('#article-title').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-url').val(),
+    category: $('#article-category').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#article-published:checked').length ? util.today() : null,
+  });
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+  $('#articles').append(articleData.toHtml());
 
   // TODO: Activate the highlighting of any code blocks:
+  $('pre code').each(function (idx, block) {
+    hljs.highlightBlock(block);
+  });
 
   // TODO: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#export-field').show();
+  $('#article-json').val(JSON.stringify(articleData) + ', ');
 };
 
-
-articleView.initIndexPage = function() {
+articleView.initIndexPage = function () {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
