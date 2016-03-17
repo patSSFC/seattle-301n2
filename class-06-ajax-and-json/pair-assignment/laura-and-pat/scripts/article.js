38ccd85cfe7b5d809ecc1cffdf85c4bbaabde8e2
Laura Sammons <lsammons@gmail.com>
2016-03-16 20:57:48 -0700

    Retrieve JSON file, cache to local storage, load data into Article.all, and render page

--- class-06-ajax-and-json/pair-assignment/laura-and-pat/scripts/article.js
+++ class-06-ajax-and-json/pair-assignment/laura-and-pat/scripts/article.js
@@ -38,6 +38,7 @@ Article.loadAll = function(rawData) {
 
   rawData.forEach(function(ele) {
     Article.all.push(new Article(ele));
+    //console.log(ele);
   })
 }
 
@@ -48,32 +49,26 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(//TODO: What do we pass in here to the .loadAll function?
+    //DONE: What do we pass in here to the .loadAll function?
+    Article.loadAll(
+      JSON.parse(localStorage.rawData)
     );
-    articleView.someFunctionToCall; //TODO: What method do we call to render the index page?
+    //DONE: What method do we call to render the index page?
+    //articleView.someFunctionToCall;
+    articleView.initIndexPage();
   } else {
-    // TODO: When we don't already have the rawData,
+    // DONE: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-
-    $.ajax({
-      type: 'GET',
-      url: '../data/hackerIpsum.json',
-      success: function(data, status, xhr) {
-        // data.forEach(function(article) {
-        //   $('#content').append('<p>' + article.title + '</p>');
-        // })
-        localStorage.rawData = data;
-      },
-      error: function(request, status, error) {
-        console.dir(request);
-        console.log('status: ' + status, 'error: ' + error);
-      }
-    }).done(function() {
-      console.log('Complete');
-    })
-
+    console.log('hello');
+    $.getJSON("../data/hackerIpsum.json", function( data ) {
+      localStorage.rawData = JSON.stringify(data);
+      Article.loadAll(
+        JSON.parse(localStorage.rawData)
+      );
+      articleView.initIndexPage();
+    });
   }
 }
