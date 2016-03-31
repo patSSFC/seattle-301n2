03467fb23921e71a20cb77791d6a467414e6534f
Pat Yolian <pyolian@gmail.com>
2016-03-30 18:58:45 -0700

    createTable and fetchAll moved to articleController.js, added script tag for articleController

--- class-11-spa-routing/pair-assignment/laura-pat/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/laura-pat/scripts/articleController.js
@@ -1,10 +1,13 @@
 (function(module) {
   var articlesController = {};
 
-  // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  // DONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
 
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  Article.createTable();
+
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
+  Article.fetchAll(articleView.initIndexPage);
   // Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
 
