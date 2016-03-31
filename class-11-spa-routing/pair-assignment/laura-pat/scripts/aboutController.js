f57000bbad8b12ea39471b17163b44534e29d1ea
Laura Sammons <lsammons@gmail.com>
2016-03-30 20:02:14 -0700

    add scripts, work on aboutController and routes

--- class-11-spa-routing/pair-assignment/laura-pat/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/laura-pat/scripts/aboutController.js
@@ -3,7 +3,8 @@
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
-
+    $('section').hide();
+    $('section#about').fadeIn();
   };
 
   module.aboutController = aboutController;
--- class-11-spa-routing/pair-assignment/laura-pat/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/laura-pat/scripts/routes.js
@@ -1,4 +1,19 @@
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
+page.base('/');
+page('/', articles);
+page('/about', aboutController.index());
+
+function articles() {
+  console.log("inside articles");
+  $('.main-nav').on('click', '.tab', function(e) {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+
+  $('.main-nav .tab:first').click();
+};
+
 
 // TODO: What function do you call to activate page.js? Fire it off now, to execute
+page();
