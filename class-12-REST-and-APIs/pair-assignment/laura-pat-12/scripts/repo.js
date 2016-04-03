160405adb62c9c98d5145a08dbfcd2995b01cfbc
Laura Sammons <lsammons@gmail.com>
2016-04-03 14:59:12 -0700

    add Ajax request

--- class-12-REST-and-APIs/pair-assignment/laura-pat-12/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/laura-pat-12/scripts/repo.js
@@ -5,7 +5,17 @@
 
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      type: 'GET',
+      headers: {
+        'Authorization': authToken,
+      },
+      success: function(data, message, xhr) {
+        console.log(data);
+        repos.all = data;
+      },
+    });
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
